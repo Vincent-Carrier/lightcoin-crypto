5db98f1cfdc93830ec2c7af5b7db43f8159fd9f2
Vincent-Carrier <vincentcarrier15@gmail.com>
2019-12-06 13:17:49 -0500

    Complete exercise

--- index.js
+++ index.js
@@ -1,29 +1,56 @@
-let balance = 500.00;
-
-class Withdrawal {
-
-  constructor(amount) {
-    this.amount = amount;
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
   }
 
-  commit() {
-    balance -= this.amount;
+  get balance() {
+    return this.transactions.reduce((a, b) => a + b, 0);
   }
 
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
 }
 
+class Transaction {
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
+  }
 
+  commit(callback) {
+    if (this.account.balance + this.value < 0) {
+      callback(new Error("Insufficient amount for withdrawal"));
+    } else {
+      this.time = new Date();
+      this.account.addTransaction(this);
+    }
+  }
+}
 
+class Deposit extends Transaction {
+  get value() {
+    return this.amount;
+  }
+}
+
+class Withdrawal extends Transaction {
+  get value() {
+    return -this.amount;
+  }
+}
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
+const cb = err => console.log("Something wrong happened:", err);
+const myAccount = new Account("snow-patrol");
+t1 = new Withdrawal(50.25, myAccount);
+t1.commit(cb);
+console.log("Transaction 1:", t1);
 
-t1 = new Withdrawal(50.25);
-t1.commit();
-console.log('Transaction 1:', t1);
+t2 = new Withdrawal(9.99, myAccount);
+t2.commit(cb);
+console.log("Transaction 2:", t2);
 
-t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
-
-console.log('Balance:', balance);
+console.log("Balance:", myAccount.balance);
